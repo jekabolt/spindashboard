395c821e627f7a9aa3aa5867fff539aac78ef28f
user <user@MacBook-Air-demo-2.local>
2021-03-23 19:03:05 +0300

    make it better

---
+++ first_dash.py
@@ -0,0 +1,10 @@
+import dash
+import pandas as pd
+import plotly.express as px
+import dash_table
+import dash_core_components as docs
+import dash_html_components as html
+from dash.dependencies import Input, Output
+
+app = dash.Dash(__name__)
+server = app.server
