6498dc490617a4e90cc6f985cbc43e54c7f28c2b
jekabolt <jekabolt@yahoo.com>
2021-04-23 18:34:58 +0300

    refactor and add goods ftp retriever

--- first_dash.py
+++ first_dash.py
@@ -9,167 +9,42 @@ import dash_html_components as html
 import dash_bootstrap_components as dbc
 from dash.dependencies import Input, Output
 import openpyxl
+import os
+import layout
+import goods
 
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 server = app.server
 
-
+# auth
 VALID_USERNAME_PASSWORD_PAIRS = {
-    'kek': 'kek'
+    os.environ.get('LOGIN_USERNAME'): os.environ.get('LOGIN_PASSWORD'),
 }
+
 auth = dash_auth.BasicAuth(
     app,
     VALID_USERNAME_PASSWORD_PAIRS
 )
 
+# layout
+app.layout = layout.get_layout()
 
-# -------------------------------------------------------------------------------------------------------------------------
-# ДАННЫЕ
-# -------------------------------------------------------------------------------------------------------------------------
-
-df = pd.read_excel('goods.xlsx', usecols="A,C:E,G,H,Z,AD,AF:AI,AN,AP", skiprows=3,
-                   names=['id', 'Status', 'Date', 'Sale_Date', 'Owner', 'Rule', 'First_Price', 'Sale_Price', 'Ship',
-                          'Pay_Cost', 'Plat_Cost', 'Recover', 'Plat', 'Seller'], engine='openpyxl',
-                   converters={'id': str, 'Sale_Price': int})
-df = df[df['Status'] == "Продажа"].drop(columns='Status')
-dict_of_plats = {None: 'Grailed',
-                 'VK': 'VK',
-                 'Avito': 'Avito',
-                 'Spin4spin': 'Grailed',
-                 'TheMarket': 'VK',
-                 'Лемаркет': 'VK',
-                 'Ebay': 'Ebay',
-                 'Spin4spin__': 'Grailed',
-                 'Buyma': 'Buyma',
-                 'Grailed': 'Grailed',
-                 'showroom': 'Showroom',
-                 'Spin4spin_': 'Grailed',
-                 'Spin4spin___': 'Grailed',
-                 'maahno': 'Grailed',
-                 'Официальный Сайт ': 'Website',
-                 'saintlaurentarchive': 'Grailed',
-                 'Instagram': 'Instagram',
-                 'Invoice': 'Grailed',
-                 'Taobao': 'Taobao',
-                 'Heroine': 'Grailed'}
-df['Plat'] = df['Plat'].replace(dict_of_plats)
-# df['Sale_Date'] = pd.to_datetime(df['Sale_Date'])
-df['sales_month'] = df['Sale_Date'].str[3:5]
-df['sales_year'] = df['Sale_Date'].str[6:]
-df['sales_day'] = df['Sale_Date'].str[:2]
-
-# -------------------------------------------------------------------------------------------------------------------------
-# HTML ВЫКЛАДКА
-# -------------------------------------------------------------------------------------------------------------------------
-
-app.layout = html.Div([
-
-    dbc.Row([
-        dbc.Col(html.H1("data range selector", style={'text-align': 'center'}),
-                width={"size": 8, "offset": 2}, )]),
-
-    dbc.Row([
-        dbc.Col(html.A("graph 1", href="#graph-1", style={'text-align': 'center'}),
-                width={"size": 8, "offset": 2}, )]),
-    dbc.Row([
-            dbc.Col(html.A("graph 2", href="#graph-2", style={'text-align': 'center'}),
-                    width={"size": 8, "offset": 2}, )]),
-
-    dbc.Row([
-        dbc.Col(html.A("graph 3", href="#graph-3", style={'text-align': 'center'}),
-                width={"size": 8, "offset": 2}, )]),
-
-    dcc.Dropdown(id="slct-year",
-                 options=[
-                     {'label': '2020', 'value': '2020'},
-                     {'label': '2021', 'value': '2021'}],
-                 multi=False,
-                 value='2021',
-                 style={'width': '40%'}
-                 ),
-    dcc.Dropdown(id="slct-month",
-                 options=[
-                     {'label': 'Январь', 'value': '01'},
-                     {'label': 'Февраль', 'value': '02'},
-                     {'label': 'Март', 'value': '03'},
-                     {'label': 'Апрель', 'value': '04'},
-                     {'label': 'Май', 'value': '05'},
-                     {'label': 'Июнь', 'value': '06'},
-                     {'label': 'Июль', 'value': '07'},
-                     {'label': 'Август', 'value': '08'},
-                     {'label': 'Сентябрь', 'value': '09'},
-                     {'label': 'Октябрь', 'value': '10'},
-                     {'label': 'Ноябрь', 'value': '11'},
-                     {'label': 'Декабрь', 'value': '12'}
-                 ],
-                 multi=False,
-                 value='03',
-                 style={'width': '40%'}
-                 ),
-
-    html.Br(),
-
-    dbc.Row([
-        dbc.Col(html.H1("total moth sales", id="graph-1", style={'text-align': 'center'}),
-                width={"size": 8, "offset": 2}, )]),
-
-    dbc.Row([
-        dbc.Col(
-            [dcc.Graph(id='results', figure={})
-             ], width={"size": 3}
-        ),
-        dbc.Col(dcc.Graph(id='sale_calendar', figure={}), width={"size": 9})
-
-    ]),
-
-
-    # --
-
-    dbc.Row([
-        dbc.Col(html.H1("total moth sales 2", id="graph-2", style={'text-align': 'center'}),
-                width={"size": 8, "offset": 2}, )]),
-
-    dbc.Row([
-        dbc.Col(
-            [dcc.Graph(id='results-2', figure={})
-             ], width={"size": 3}
-        ),
-        dbc.Col(dcc.Graph(id='sale_calendar-2', figure={}), width={"size": 9})
-    ]),
-
-    # --
-
-    dbc.Row([
-        dbc.Col(html.H1("total moth sales 3", id="graph-3", style={'text-align': 'center'}),
-                width={"size": 8, "offset": 2}, )]),
-
-    dbc.Row([
-        dbc.Col(
-            [dcc.Graph(id='results-3', figure={})
-             ], width={"size": 3}
-        ),
-        dbc.Col(dcc.Graph(id='sale_calendar-3', figure={}), width={"size": 9})
-    ]),
-
-    html.Br(),
-])
+df = goods.get_goods_df()
 
 
 # --------------------------------------------------
 # Наполнение элементов СМЫСЛОМ
 #
+
+
 @app.callback(
     [Output(component_id='sale_calendar', component_property='figure'),
      Output(component_id='results', component_property='figure'),
-     #  Output(component_id='sale_calendar-2', component_property='figure'),
-     #  Output(component_id='results-2', component_property='figure'),
-
-
      ],
     [Input(component_id='slct-year', component_property='value'),
      Input(component_id='slct-month', component_property='value')]
 )
-def update_graph(year, month):
+def update_graph_live(year, month):
     dff = df.copy()
     print(month, year)
     dff = dff[(dff['sales_year'] == year) & (dff['sales_month'] == month)]
