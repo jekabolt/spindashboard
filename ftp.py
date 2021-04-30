290cc3707a23eea7a04a9477213f914c12563370
jekabolt <jekabolt@yahoo.com>
2021-05-01 01:02:08 +0300

    get latest ftp file

--- ftp.py
+++ ftp.py
@@ -1,6 +1,7 @@
 from ftplib import FTP
 import datetime
 import os
+from dateutil import parser
 
 
 def get_goods(local_goods_file_path="goods.xlsx"):
@@ -16,8 +17,24 @@ def get_goods(local_goods_file_path="goods.xlsx"):
 
     now = datetime.datetime.now()
 
+    names = ftp.nlst()
+
+    latest_time = None
+    latest_name = None
+
+    for name in names:
+        time = ftp.voidcmd("MDTM " + name)
+        if (latest_time is None) or (time > latest_time):
+            latest_name = name
+            latest_time = time
+
+    print(latest_name)
+
+    print("\n\n keek ", latest_name)
+
     # ftp_file_name = now.strftime("%y_%m_%d")+".xlsx"
-    ftp_file_name = "21_04_28.xlsx"
+    # ftp_file_name = "21_04_28.xlsx"
+    ftp_file_name = latest_name
     with open(local_goods_file_path, 'wb') as local_goods_file_path:
         ftp.retrbinary('retr ' + ftp_file_name, local_goods_file_path.write)
     ftp.close()
